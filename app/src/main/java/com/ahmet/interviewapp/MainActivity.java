1f7512094cf4f3d45f073ead306fbdfdb5436e14
Legacy07 <deadmanwalking2007@hotmail.com>
2018-11-26 21:39:00 +0000

    Completed the initial version.
    Updated the following files.
    Also redesigned the layouts.

--- app/src/main/java/com/ahmet/interviewapp/MainActivity.java
+++ app/src/main/java/com/ahmet/interviewapp/MainActivity.java
@@ -1,11 +1,14 @@
 package com.ahmet.interviewapp;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -42,6 +45,17 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        //open interview fragment on launch
+        if (savedInstanceState == null) {
+            //open interview fragment
+            InterviewFragment interviewFragment = new InterviewFragment();
+            //replacing the fragment with the layout
+            manager.beginTransaction().replace(R.id.content_layout, interviewFragment).addToBackStack(null).commit();
+
+        }
+//        Interview fragment checked on nav
+        navigationView.getMenu().getItem(0).setChecked(true);
+
         //to start nav bar from right
 //        getWindow().getDecorView().setLayoutDirection(View.LAYOUT_DIRECTION_RTL);
     }
@@ -52,8 +66,56 @@ public class MainActivity extends AppCompatActivity
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
-            super.onBackPressed();
+            //if it hits the activity stack then ask for exit
+            int fragments = getSupportFragmentManager().getBackStackEntryCount();
+            if (fragments == 1) {
+
+                //exit application
+                new AlertDialog.Builder(this).setIcon(R.drawable.ic_alert).setTitle("Exit")
+                        .setMessage("Exit the application?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Intent intent = new Intent(Intent.ACTION_MAIN);
+                                intent.addCategory(Intent.CATEGORY_HOME);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                                System.gc();
+                                System.exit(0);
+                            }
+                        }).setNegativeButton("Cancel", (null)).show();
+            } else {
+                if (getFragmentManager().getBackStackEntryCount() > 1) {
+                    getFragmentManager().popBackStack();
+                } else {
+                    super.onBackPressed();
+                }
+            }
+
         }
+
+//        //exit application
+//        new AlertDialog.Builder(this).setIcon(android.R.drawable.ic_dialog_alert).setTitle("Exit")
+//                .setMessage("Are you sure?")
+//                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialogInterface, int i) {
+//                        Intent intent = new Intent(Intent.ACTION_MAIN);
+//                        intent.addCategory(Intent.CATEGORY_HOME);
+//                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//                        startActivity(intent);
+//                        System.gc();
+//                        System.exit(0);
+//                    }
+//                }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialogInterface, int i) {
+//                //open interview fragment
+//                InterviewFragment interviewFragment = new InterviewFragment();
+//                //replacing the fragment with the layout
+//                manager.beginTransaction().replace(R.id.content_layout, interviewFragment).addToBackStack(null).commit();
+//            }
+//        }).show();
 
     }
 
@@ -112,9 +174,7 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.references) {
             References references = new References();
             manager.beginTransaction().replace(R.id.content_layout, references).addToBackStack(null).commit();
-        }
-
-        else if (id == R.id.blog) {
+        } else if (id == R.id.blog) {
             BlogFragment blog = new BlogFragment();
             manager.beginTransaction().replace(R.id.content_layout, blog).addToBackStack(null).commit();
         }
